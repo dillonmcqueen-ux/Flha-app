47026c704d18714a24e8a7656f91c1cbbd361524
dillonmcqueen-ux <dillonmcqueen@hotmail.com>
2026-07-07 15:33:40 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Dashboard from './Dashboard.jsx'
-
-const isDashboard = window.location.pathname === '/dashboard'
+import Login from './Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {isDashboard ? <Dashboard /> : <App />}
+    <Login />
   </React.StrictMode>,
 )
